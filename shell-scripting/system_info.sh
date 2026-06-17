44059459375245906ace45981a832c4d3df92325
ArbuthaDurairaj <dura0118@algonquinlive.com>
2026-06-17 15:48:45 -0400

    Create system_info.sh for displaying system info
    Added a script to display system information including hostname, current user, date, kernel version, disk usage, memory usage, and system uptime.

--- shell-scripting/system_info.sh
+++ shell-scripting/system_info.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+echo "==============================="
+echo " Linux System Information"
+echo "==============================="
+
+echo "Hostname      : $(hostname)"
+echo "Current User  : $(whoami)"
+echo "Current Date  : $(date)"
+echo "Kernel Version: $(uname -r)"
+
+echo
+echo "Disk Usage:"
+df -h
+
+echo
+echo "Memory Usage:"
+free -h
+
+echo
+echo "System Uptime:"
+uptime
